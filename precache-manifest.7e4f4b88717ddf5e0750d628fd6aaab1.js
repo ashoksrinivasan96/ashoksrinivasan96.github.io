de48396d11932962b9cc3715d42bb99abd30f173
G. Ashok Srinivasan <ashoksrinivasan96@gmail.com>
2020-09-19 23:00:28 +0530

    Updates

---
+++ precache-manifest.7e4f4b88717ddf5e0750d628fd6aaab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6701d0e261dda11f747346b423799526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc38ee31ee0a74eced6",
+    "url": "/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "8fa05a725ce0020bc0c0",
+    "url": "/static/js/2.4a91bcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a91bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc38ee31ee0a74eced6",
+    "url": "/static/js/main.a96e1a63.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);

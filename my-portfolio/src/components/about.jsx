ab89ea851977a3e40f10106379ec8dddb58c39af
G. Ashok Srinivasan <ashoksrinivasan96@gmail.com>
2020-09-28 19:29:30 +0530

    UPDATE: Fixed grammar

--- my-portfolio/src/components/about.jsx
+++ my-portfolio/src/components/about.jsx
@@ -27,7 +27,7 @@ export default class About extends Component {
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                 <span className="heading-meta">What do I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                <h2 className="colorlib-heading">Here are some of my top skills</h2>
             </div>
             </div>
             <div className="row row-pt-md">

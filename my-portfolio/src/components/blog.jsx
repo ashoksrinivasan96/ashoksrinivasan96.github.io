f7d56d4cafc9902023e2fa5d648ebf091a1d2a05
G. Ashok Srinivasan <ashoksrinivasan96@gmail.com>
2020-09-29 19:19:44 +0530

    UPDATE: Blog text changes

--- my-portfolio/src/components/blog.jsx
+++ my-portfolio/src/components/blog.jsx
@@ -8,7 +8,7 @@ export default class Blog extends Component {
 			<div className="colorlib-narrow-content">
 				<div className="row">
 					<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-					<span className="heading-meta">Read</span>
+					<span className="heading-meta">Read my</span>
 					<h2 className="colorlib-heading">Recent Blog</h2>
 					</div>
 				</div>
